3a2a398b06018fe17828caa829b329335224ea76
carlrcloud <yontacarlos@yahoo.fr>
2024-02-09 23:14:35 -0500

    calculator

---
+++ project1/sum_calculator.py
@@ -0,0 +1,17 @@
+'''
+write a python that will ask for two numbers (interger or/and float)
+calculate the sum of those numbers
+
+'''
+
+#ask for the first number
+number1 = int(input("Please give me a random number:\n"))
+
+#ask for the second number
+number2 = int(input("Please give me another random number:\n"))
+
+# calculation of the sum of the numbers
+
+sum = number1 + number2
+
+print(f"The sum of {number1} and {number2}  is: {sum}")
