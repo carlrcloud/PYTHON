172cfd1b082b66be4a5ba33df26b8417ce142a2d
carlrcloud <yontacarlos@yahoo.fr>
2024-02-12 01:09:44 -0500

    reversing a number

--- project1/aws_account_id.py
+++ project1/aws_account_id.py
@@ -5,9 +5,9 @@ This program will extract a AWS account ID from a resource ARN
 # The reource ARN
 ARN = "arn:aws:iam::123456789012:uer/Development/product_1234/*"
 lenght = len(ARN)
-print(f"The lentgh of the ARN is: {lenght}")
+print(f"The lentgh of the ARN is: {lenght}.")
 
 # Let's extract the account ID from this arn
 
 account_Id = ARN[13:25]
-print(f"The AWS account ID that content this resource is: {account_Id}")
+print(f"The AWS account ID that content this resource is: {account_Id}.")
---
+++ project1/reversing.py
@@ -0,0 +1,9 @@
+"""
+Let's write a program that will ask for a word and reverse it.
+"""
+# ask user to enter a word
+word = input("Please give a rendon word:\n")
+
+# reversing the word 
+reversed_word = word[::-1]
+print(f"The reverse of {word} is {reversed_word}")
